ab8916f2c6ccf21aa13a2e59b1b7c0307ec2f881
Srijan Shovit <srijanshovit@gmail.com>
2021-12-02 13:07:30 +0530

    navigating programtically

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
 import Link from 'next/link'
 
+//useRouter is used for navigating programatically
+import {useRouter} from 'next/router'
 export default function Home() {
+  const router = useRouter()
+  const handler = () => {
+    //the push method of router object is used 
+    // router.push('/product')   //pushes into stack
+    router.replace('/product')   //replaces into stack
+  }
   return (
     <div >
      Home page
      <Link href="/blog">
       <a>Blog</a>
      </Link>
+
+     {/* button to navigate to product list page */}
+     <button onClick={handler}>
+       View all products
+     </button>
     </div>
   )
 }
---
+++ pages/product/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProductList = () => {
+    return (
+        <div>
+            P1
+            P2
+            P3
+        </div>
+    )
+}
+
+export default ProductList
