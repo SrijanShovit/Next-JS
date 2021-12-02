79e467dde18584baf57c0af092592fe441b2f4f2
Srijan Shovit <srijanshovit@gmail.com>
2021-12-02 10:19:00 +0530

    dynamic routes

---
+++ pages/product/sweater.js
@@ -0,0 +1,8 @@
+function Sweater(){
+    return <h1>Landing Page for sweaters</h1>
+}
+
+export default Sweater
+
+//even though we have a dynamic route whose value could be anything including sweater
+//next js prefers specific route named sweater
