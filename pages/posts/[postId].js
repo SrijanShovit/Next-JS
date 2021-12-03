8962ebeb1b892274a195c8627134967d84b358cd
Srijan Shovit <srijanshovit@gmail.com>
2021-12-03 09:41:48 +0530

    fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,5 +1,15 @@
+import {useRouter} from 'next/router'
+
 function Post({post}){
+    const router = useRouter()
+
+    //for pages not generated at build time this fallback version is shown then the page is  statically generated if fallback is true
+    if (router.isFallback){
+        return <h1>Loading...</h1>
+    }
     console.log(post);
+
+    //this is returned when fallback becomes false again
     return(
         <>
             <h2>{post.title}</h2>
@@ -18,30 +28,32 @@ export async function getStaticPaths(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json()
 
-    const paths = data.map(post => {
-        return {
-            params:{
-                postId : `${post.id}`
-            }
-        }
-    })
+    // const paths = data.map(post => {
+    //     return {
+    //         params:{
+    //             postId : `${post.id}`
+    //         }
+    //     }
+    // })
     return {
         //this key tells what pages are statically generated at build time
         //it is an array of object
-        // paths: [
-        //     {//each object has parameters key which is object 
-        //         //specifying the path id
-        //         //the id must be string
-        //         //3 objects as we have asked for 3 pgs
-        //         params:{ postId : '1'}
-        //     },{
-        //         params:{ postId : '2'}
-        //     },{
-        //         params:{ postId : '3'}
-        //     }
-        // ] ,
-        paths: paths,
-        fallback:false
+        paths: [
+            {//each object has parameters key which is object 
+                //specifying the path id
+                //the id must be string
+                //3 objects as we have asked for 3 pgs
+
+                //if all pages are pre-fetched and only three build statically then also loading won't appear ; rather on scroll or click pages will be statically generated
+                params:{ postId : '1'}
+            },{
+                params:{ postId : '2'}
+            },{
+                params:{ postId : '3'}
+            }
+        ] ,
+        // paths: paths,
+        fallback:true
     }
 }
 
@@ -50,7 +62,15 @@ export async function getStaticProps(context){
     //we get hold of dynamic parameters insdie getStaticProps using params of context props
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await res.json()
-    // console.log(data)
+
+    //if requets for id>100 made return notfound object = true
+    if (!data.id) {
+        return {
+            notFound: true
+        }
+    }
+    console.log(`Generating page for /posts/${params.postId}`)
+    
     return {
         props:{
             post : data
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,6 @@
 //useEffect hook won't allow pre-rendering
+
+
 import Link from 'next/link'
 function PostList({posts}){
     return(
@@ -25,6 +27,8 @@ export default PostList
 export async function getStaticProps(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json()
+
+   
 
     return {
         props:{
